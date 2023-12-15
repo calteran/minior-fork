6e934284ec3e6e4496b655129c6aaeaeafbfbab1
Robert Lopez <robertlopezca@gmail.com>
2023-12-14 19:55:20 -0800

    Implement delete_object_presigned

--- src/core/delete.rs
+++ src/core/delete.rs
@@ -1,7 +1,11 @@
 // Authors: Robert Lopez
 
 use crate::error::Error;
-use aws_sdk_s3::Client;
+use aws_sdk_s3::{
+    presigning::{PresignedRequest, PresigningConfig},
+    Client,
+};
+use std::time::Duration;
 
 pub async fn delete_object(
     client: &Client,
@@ -17,4 +21,28 @@ pub async fn delete_object(
         .map_err(|err| Error::sdk(err))?;
 
     Ok(())
+}
+
+pub async fn delete_object_presigned(
+    client: &Client,
+    bucket_name: &str,
+    object_name: &str,
+    presigned_expiry: Option<u64>,
+) -> Result<PresignedRequest, Error> {
+    let presigning_config = if let Some(expiration_seconds) = presigned_expiry {
+        PresigningConfig::builder()
+            .expires_in(Duration::from_secs(expiration_seconds))
+            .build()
+    } else {
+        PresigningConfig::builder().build()
+    }
+    .map_err(|err| Error::sdk(err))?;
+
+    Ok(client
+        .delete_object()
+        .bucket(bucket_name)
+        .key(object_name)
+        .presigned(presigning_config)
+        .await
+        .map_err(|err| Error::sdk(err))?)
 }
