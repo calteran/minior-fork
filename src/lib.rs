9435093f1b20e912dc1055ad19c0458ac5ae0ea2
Robert Lopez <robertlopezca@gmail.com>
2023-12-22 15:26:29 -0800

    Move Minio to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,239 @@
+// Authors: Robert Lopez
+
 pub mod core;
 pub mod error;
-pub mod minio;
+
+use crate::{
+    core::{
+        bucket::*,
+        delete::*,
+        get::*,
+        upload::{upload_object::*, upload_object_presigned::PresignedUploadManager},
+    },
+    error::Error,
+};
+use aws_sdk_s3::{presigning::PresignedRequest, types::Bucket, Client};
+use std::sync::Arc;
+use tokio::io::{AsyncBufRead, AsyncRead};
+
+/// Minio client utilizing the S3 API
+///
+/// ---
+/// Example Usage:
+/// ```
+///
+/// // Note: Provide the S3 API Port
+/// let url = "http://127.0.0.1:9000";
+///
+/// let bucket_name = "shark_images";
+///
+/// let minio = Minio::new(url).await?;
+///
+/// minio.create_bucket(bucket_name).await?;
+///
+/// let shark_image: tokio::fs::File = ...;
+/// let object_name = "shark.jpg";
+///
+/// minio.upload_object(
+///       bucket_name,
+///       object_name,
+///       shark_image,
+///       None,
+///       None,
+///   )
+///   .await?;
+///
+/// let request: PresignedRequest = minio.get_object_presigned(
+///     bucket_name,
+///     object_name,
+///     Some(3_600),
+/// ).await?;
+/// ```
+pub struct Minio {
+    pub client: Arc<Client>,
+}
+
+impl Minio {
+    /// Constructs a new Minio client from the S3 API Url
+    ///
+    /// Loads credentials from environment
+    ///
+    /// ---
+    /// Example Usage:
+    /// ```
+    ///
+    /// // Note: Provide the S3 API Port
+    /// let url = "http://127.0.0.1:9000";
+    ///
+    /// let minio = Minio::new(url).await?;
+    /// ```
+    pub async fn new(url: &str) -> Self {
+        let config = aws_config::from_env().endpoint_url(url).load().await;
+        let client = Client::new(&config);
+
+        Self {
+            client: Arc::new(client),
+        }
+    }
+
+    /// Returns true if a bucket by `bucket_name` exists
+    ///
+    /// ---
+    /// Example Usage:
+    /// ```
+    ///
+    /// let minio: Minio = ...;
+    ///
+    /// if minio.bucket_exists("sharks").await? {
+    ///     ...
+    /// }
+    /// ```
+    pub async fn bucket_exists(&self, bucket_name: &str) -> Result<bool, Error> {
+        bucket_exists(&self.client, bucket_name).await
+    }
+
+    /// Returns a vector of `Bucket`s from the client
+    ///
+    /// ---
+    /// Example Usage:
+    /// ```
+    ///
+    /// let minio: Minio = ...;
+    ///
+    /// for bucket in minio.list_buckets().await? {
+    ///     ...
+    /// }
+    /// ```
+    pub async fn list_buckets(&self) -> Result<Vec<Bucket>, Error> {
+        list_buckets(&self.client).await
+    }
+
+    /// Creates a new bucket named `bucket_name`
+    ///
+    /// Returns `false` if bucket already existed
+    ///
+    /// ---
+    /// Example Usage:
+    /// ```
+    ///
+    /// let minio: Minio = ...;
+    ///
+    /// let bucket_created: bool = minio.create_bucket("sharks").await?;
+    /// ```
+    pub async fn create_bucket(&self, bucket_name: &str) -> Result<bool, Error> {
+        create_bucket(&self.client, bucket_name).await
+    }
+
+    /// Deletes a bucket by `bucket_name`
+    ///
+    /// Returns `false` if the bucket did not exist
+    ///
+    /// ---
+    /// Example Usage:
+    /// ```
+    ///
+    /// let minio: Minio = ...;
+    ///
+    /// let bucket_deleted: bool = minio.delete_bucket("sharks").await?;
+    /// ```
+    pub async fn delete_bucket(&self, bucket_name: &str) -> Result<bool, Error> {
+        delete_bucket(&self.client, bucket_name).await
+    }
+
+    pub async fn get_object(
+        &self,
+        bucket_name: &str,
+        object_name: &str,
+    ) -> Result<impl AsyncBufRead, Error> {
+        get_object(&self.client, bucket_name, object_name).await
+    }
+
+    pub async fn get_object_presigned(
+        &self,
+        bucket_name: &str,
+        object_name: &str,
+        presigned_expiry: Option<u64>,
+    ) -> Result<PresignedRequest, Error> {
+        get_object_presigned(&self.client, bucket_name, object_name, presigned_expiry).await
+    }
+
+    pub async fn upload_object<S>(
+        &self,
+        bucket_name: &str,
+        object_name: &str,
+        stream: S,
+        buffer_size: Option<usize>,
+        data_part_size: Option<usize>,
+    ) -> Result<(), Error>
+    where
+        S: AsyncRead + Unpin,
+    {
+        upload_object(
+            &self.client,
+            bucket_name,
+            object_name,
+            stream,
+            buffer_size,
+            data_part_size,
+        )
+        .await
+    }
+
+    pub async fn upload_object_presigned<'uop>(
+        &self,
+        bucket_name: &'uop str,
+        object_name: &'uop str,
+        presigned_expiry: Option<u64>,
+    ) -> Result<PresignedUploadManager<'uop>, Error> {
+        PresignedUploadManager::new(&self.client, bucket_name, object_name, presigned_expiry).await
+    }
+
+    pub async fn delete_object(&self, bucket_name: &str, object_name: &str) -> Result<(), Error> {
+        delete_object(&self.client, bucket_name, object_name).await
+    }
+
+    pub async fn delete_object_presigned(
+        &self,
+        bucket_name: &str,
+        object_name: &str,
+        presigned_expiry: Option<u64>,
+    ) -> Result<PresignedRequest, Error> {
+        delete_object_presigned(&self.client, bucket_name, object_name, presigned_expiry).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+    use tokio::fs::*;
+
+    #[tokio::test]
+    async fn test_new() {
+        let mut rng = rand::thread_rng();
+
+        let url = "http://127.0.0.1:9000";
+
+        let minio = Minio::new(url).await;
+
+        let mut data = vec![];
+
+        for _ in 0..10_000_000 {
+            data.push(rng.gen_range(0..255) as u8);
+        }
+
+        let file_path = "./test.txt";
+        tokio::fs::write(file_path, data).await.unwrap();
+
+        let file = File::open(file_path).await.unwrap();
+
+        let bucket_name = "test";
+
+        minio.create_bucket(bucket_name).await.unwrap();
+
+        minio
+            .upload_object(bucket_name, "test.txt", file, None, None)
+            .await
+            .unwrap();
+    }
+}
