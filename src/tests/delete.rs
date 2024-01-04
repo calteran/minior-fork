98c4e03183559f5d99c6f9d09ff919d93ac52c73
Robert Lopez <robertlopezca@gmail.com>
2024-01-04 11:38:24 -0800

    Add delete unit tests

---
+++ src/tests/delete.rs
@@ -0,0 +1,68 @@
+// Authors: Robert Lopez
+
+use super::util::{test_client::TestClient, *};
+
+#[tokio::test]
+async fn test_delete() {
+    let object_name = "shark.png";
+    let test_client = TestClient::new().await;
+
+    test_client
+        .run_test(|minio, bucket_name| async move {
+            let file = get_test_file(object_name).await?;
+
+            minio
+                .upload_object(&bucket_name, object_name, file, None, None)
+                .await?;
+
+            minio.delete_object(&bucket_name, object_name).await?;
+
+            assert_object(
+                &minio,
+                &bucket_name,
+                object_name,
+                ObjectAssertions::DoesNotExist,
+            )
+            .await?;
+
+            Ok(())
+        })
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn test_delete_presigned() {
+    let object_name = "shark.png";
+    let test_client = TestClient::new().await;
+
+    test_client
+        .run_test(|minio, bucket_name| async move {
+            let file = get_test_file(object_name).await?;
+            let reqwest_client = reqwest::Client::new();
+
+            minio
+                .upload_object(&bucket_name, object_name, file, None, None)
+                .await?;
+
+            let request = minio
+                .delete_object_presigned(&bucket_name, object_name, 1_337)
+                .await?;
+
+            let delete_url = request.uri();
+
+            reqwest_client.delete(delete_url).send().await?;
+
+            assert_object(
+                &minio,
+                &bucket_name,
+                object_name,
+                ObjectAssertions::DoesNotExist,
+            )
+            .await?;
+
+            Ok(())
+        })
+        .await
+        .unwrap();
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,2 +1,3 @@
+pub mod delete;
 pub mod upload_get;
 pub mod util;
